c6c787df07bd1ccb8eaa8df0c2ba8b15ca48fe40
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-03 09:36:18 +0300

    using query service in client

--- blog-boilerplate/client/src/CommentList.js
+++ blog-boilerplate/client/src/CommentList.js
@@ -1,20 +1,4 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-
-export default function CommentList({ postId }) {
-  const [comments, setComments] = useState([]);
-
-  async function fetchData() {
-    const result = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(result.data);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+export default function CommentList({ comments }) {
 
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
