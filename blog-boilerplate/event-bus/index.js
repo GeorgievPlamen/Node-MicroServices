1941819686ad558e8b09a68abf36c8f55f783113
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-03 09:03:32 +0300

    emitting events

--- blog-boilerplate/event-bus/index.js
+++ blog-boilerplate/event-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post("http://localhost:4000/events", event).catch(e => console.log(e));
-    axios.post("http://localhost:4001/events", event).catch(e => console.log(e));
-    axios.post("http://localhost:4002/events", event).catch(e => console.log(e));
+    console.log("Posting event: ", event);
+    axios.post("http://localhost:4000/events", event).catch(e => console.log(e.message));
+    axios.post("http://localhost:4001/events", event).catch(e => console.log(e.message));
+    axios.post("http://localhost:4002/events", event).catch(e => console.log(e.message));
 
     res.send({ status: "OK" });
 })
