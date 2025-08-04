f62b990d74dbcf6cc444f0afe0311b6f436129ae
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-04 07:52:02 +0300

    add comment updated to query

--- blog-boilerplate/query/index.js
+++ blog-boilerplate/query/index.js
@@ -21,10 +21,20 @@ app.post("/events", (req, res) => {
 
     if (type === "CommentCreated") {
         const { id, content, postId, status } = data;
-
         const post = posts[postId];
 
         post.comments.push({ id, content, status });
+
+        res.send({});
+    }
+
+    if (type === "CommentUpdated") {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        const comment = post.comments.find(c => c.id === id);
+
+        comment.content = content;
+        comment.status = status;
 
         res.send({});
     }
