96386b2ed13d092402804501e7a845d09dd54dab
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-04 07:44:28 +0300

    moderation service

---
+++ blog-boilerplate/moderation/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { randomBytes } from "crypto";
+import bodyParser from "body-parser";
+import cors from 'cors';
+import axios from "axios";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.post("/events", async (req, res) => {
+    console.log("Received Event: ", req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post("http://localhost:5005/events", {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+
+    res.send({});
+})
+
+app.listen(4003, () => {
+    console.log("Listening on 4003.");
+});
