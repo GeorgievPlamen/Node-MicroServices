87e13d142f8c84b2c347019100e230bc64044faa
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-09 09:29:48 +0300

    Add error handler middleware

--- ticketing-project/auth/src/routes/signup.ts
+++ ticketing-project/auth/src/routes/signup.ts
@@ -16,12 +16,13 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.status(400).send(errors.array());
+      throw new Error("Invalid email or password");
     }
 
     const { email, password } = req.body;
 
     console.log("Creating a user...");
+
     res.send({});
   }
 );
