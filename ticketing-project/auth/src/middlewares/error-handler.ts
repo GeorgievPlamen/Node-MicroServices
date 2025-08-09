0e925e2d6ccbb8b3656d4852ac3e7693fe0ef990
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-09 09:59:05 +0300

    Added abstract custom error base class, not found error.

--- ticketing-project/auth/src/middlewares/error-handler.ts
+++ ticketing-project/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export function errorHandler(
   err: Error,
@@ -8,23 +7,8 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === "field") {
-        return { message: error.msg, field: error.path };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    res.status(500).send({
-      errors: [
-        {
-          message: err.message,
-        },
-      ],
-    });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
