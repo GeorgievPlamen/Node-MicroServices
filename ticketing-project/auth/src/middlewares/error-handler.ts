a3bb40a403346a1a1ea843697b373597425ee958
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-09 09:38:34 +0300

    Adding custom errors

--- ticketing-project/auth/src/middlewares/error-handler.ts
+++ ticketing-project/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export function errorHandler(
   err: Error,
@@ -6,9 +8,30 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  console.log("Something went wrong", err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    res.status(500).send({
+      errors: [
+        {
+          message: err.message,
+        },
+      ],
+    });
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [
+      {
+        message: err.message,
+      },
+    ],
   });
 }
