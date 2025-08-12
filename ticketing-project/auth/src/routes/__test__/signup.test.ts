01d74a46ba08a2f361f93aab3de3a4130837d0ba
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-12 09:39:45 +0300

    Adding more tests

--- ticketing-project/auth/src/routes/__test__/signup.test.ts
+++ ticketing-project/auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,27 @@ it("returns a 201 on successful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test213123",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "1",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  return request(app).post("/api/users/signup").send({}).expect(400);
+});
