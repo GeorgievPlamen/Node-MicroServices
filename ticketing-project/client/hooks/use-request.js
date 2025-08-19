a8092b233e26b2c132521db2e70f7f796e8abc5f
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-19 07:25:21 +0300

    useRequest hook

---
+++ ticketing-project/client/hooks/use-request.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function useRequest({ url, method, body }) {
+    const [errors, setErrors] = useState(null);
+
+    async function doRequest() {
+        try {
+            const response = await axios[method](url, body);
+            return response.data;
+        } catch (error) {
+            setErrors(
+                <div className="alert alert-danger">
+                    <h4>Oops...</h4>
+                    <ul className="my-0">
+                        {error.response.data.errors.map((err) => (
+                            <li key={err.message}>{err.message}</li>
+                        ))}
+                        ;
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+    return { doRequest, errors };
+}
