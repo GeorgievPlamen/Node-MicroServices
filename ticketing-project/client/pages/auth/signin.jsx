c1afb88e1a12997cfe54c627d18847ba1d1286be
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-19 08:23:33 +0300

    Sign in

---
+++ ticketing-project/client/pages/auth/signin.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+
+function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: { email, password },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className="form-group">
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+}
+export default Signin;
