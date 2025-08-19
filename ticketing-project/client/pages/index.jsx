cd661a2547cf015382a69228408ff21f3ea8acdf
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-19 08:19:44 +0300

    Build client wrapper for axios

--- ticketing-project/client/pages/index.jsx
+++ ticketing-project/client/pages/index.jsx
@@ -1,4 +1,16 @@
-function Index() {
-  return <div>Index 23 45</div>;
-}
-export default Index;
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default LandingPage;
