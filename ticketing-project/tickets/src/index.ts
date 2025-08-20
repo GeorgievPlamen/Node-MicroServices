c52e6f6df4ff5724a0830b51b6e4c07202cfd0a2
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-20 08:48:30 +0300

    Setup tickets srv

---
+++ ticketing-project/tickets/src/index.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY is not defined!");
+  }
+
+  try {
+    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets");
+    console.log("Connected to Mongo.");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => console.log("Listening on port 3000."));
+};
+
+(async () => await start())();
