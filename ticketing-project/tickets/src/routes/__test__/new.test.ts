5e1328c7e9b294f9ba726a8443ef3faeb3d50d6a
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-21 08:55:27 +0300

    Adding route

--- ticketing-project/tickets/src/routes/__test__/new.test.ts
+++ ticketing-project/tickets/src/routes/__test__/new.test.ts
@@ -1,22 +1,16 @@
-import { Request } from "supertest";
+import request from "supertest";
 import { app } from "../../app";
 
-it('has a route handler listening to /api/tickets for post requests', async () => {
-    
-})
+it("has a route handler listening to /api/tickets for post requests", async () => {
+  const response = await request(app).post("/api/tickets").send({});
 
-it('can only be accessed if the user is signed in', async () => {
+  expect(response.status).not.toEqual(404);
+});
 
-})
+it("can only be accessed if the user is signed in", async () => {});
 
-it('returns an error if an invalid title is provided', async () => {
+it("returns an error if an invalid title is provided", async () => {});
 
-})
+it("return an error if an invalid price is provided", async () => {});
 
-it('return an error if an invalid price is provided', async () => {
-
-})
-
-it('creates a ticket with valid inputs', async () => {
-
-})
+it("creates a ticket with valid inputs", async () => {});
