cdd5daff591620b1d4b736213cebb7d2e318857f
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-22 07:59:18 +0300

    Adding the rest of the tests and implementation of create ticket

--- ticketing-project/tickets/src/routes/new.ts
+++ ticketing-project/tickets/src/routes/new.ts
@@ -1,9 +1,34 @@
+import { requireAuth, validateRequest } from "@gp-tickets/common";
+import { body } from "express-validator";
 import express, { Request, Response } from "express";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
-router.post("/api/tickets", (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .not()
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater then 0"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  }
+);
 
 export { router as createTicketRouter };
--- ticketing-project/tickets/src/test/setup.ts
+++ ticketing-project/tickets/src/test/setup.ts
@@ -1,9 +1,10 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 let mongo: any;
 beforeAll(async () => {
-    process.env.JWT_KEY = "testkey";
+  process.env.JWT_KEY = "testkey";
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
@@ -26,3 +27,21 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+declare global {
+  var signin: () => string[];
+}
+
+global.signin = () => {
+  const payload = {
+    id: "123123",
+    email: "test@test.com",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
+};
