cfa93b5f0b9c1a062a4e0890fcfbf691cb778e34
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-22 08:29:25 +0300

    Updating common lib with error logs
    Adding show route + tests

---
+++ ticketing-project/tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,26 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+it("returns a 404 if the ticket is not found ", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .get("/api/tickets/" + id)
+    .send()
+    .expect(404);
+});
+
+it("returns the ticket if the ticket is found", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "test", price: 20 })
+    .expect(201);
+
+  const { id } = response.body;
+
+  await request(app)
+    .get("/api/tickets/" + id)
+    .send()
+    .expect(200);
+});
