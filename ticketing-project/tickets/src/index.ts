c2b41ee77ff2755db6b043df6e793d46285ac679
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-24 10:21:58 +0300

    Changin to env variables

--- ticketing-project/tickets/src/index.ts
+++ ticketing-project/tickets/src/index.ts
@@ -9,16 +9,29 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI is not defined!");
   }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID is not defined!");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("MONGO_URI is not defined!");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL is not defined!");
+  }
 
   try {
-    await natsWrapper.connect("ticketing", "random123", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on("close", () => {
       console.log("NATS is attempting to close.");
       process.exit();
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-    
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to Mongo.");
   } catch (err) {
