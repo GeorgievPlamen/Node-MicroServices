801a6d1adbd3e18d1bf308650aeb381205ad7c6b
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-28 08:38:32 +0300

    order cancelled listener

--- ticketing-project/tickets/src/events/listeners/order-cancelled-listener.ts
+++ ticketing-project/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,14 +1,14 @@
 import { Subjects } from "@gp-tickets/common";
 import Listener from "@gp-tickets/common/build/events/base/base-listener";
 import { Message } from "node-nats-streaming";
-import OrderCreatedEvent from "@gp-tickets/common/build/events/order-created-event";
 import { OrderStatus } from "@gp-tickets/common/build/events/types/order-status";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
 import TicketUpdatedPublisher from "../publishers/ticket-updated-publisher";
+import OrderCancelledEvent from "@gp-tickets/common/build/events/order-cancelled-event";
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-  readonly subject = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
 
   async onMessage(
@@ -23,7 +23,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     msg: Message
   ) {
     const ticket = await Ticket.findById(data.ticket.id);
-    ticket?.set({ orderId: data.id });
+    ticket?.set({ orderId: undefined });
     await ticket?.save();
 
     new TicketUpdatedPublisher(this.client).publish({
