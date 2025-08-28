961a0a7a2d1ffc19a6577624a02fb94aa4d6cb55
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-28 07:50:26 +0300

    add version to events

--- ticketing-project/common/src/events/ticket-updated-event.ts
+++ ticketing-project/common/src/events/ticket-updated-event.ts
@@ -4,6 +4,7 @@ export default interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
