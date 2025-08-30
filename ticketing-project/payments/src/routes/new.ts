cfc9988f13449b2e2fdaf8c27c235f547a13c5f7
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-08-30 09:42:04 +0300

    Charging with stripe and saving payment to db

--- ticketing-project/payments/src/routes/new.ts
+++ ticketing-project/payments/src/routes/new.ts
@@ -1,6 +1,16 @@
-import { requireAuth, validateRequest } from "@gp-tickets/common";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@gp-tickets/common";
 import { body } from "express-validator";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import { OrderStatus } from "@gp-tickets/common/build/events/types/order-status";
+import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -14,6 +24,28 @@ router.post(
   validateRequest,
   async (req: Request, res: Response) => {
     const { token, orderId } = req.body;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Order is cancelled!");
+    }
+
+    const charge = await stripe.charges.create({
+      currency: "eur",
+      amount: order.price * 100, // convert to cents
+      source: token,
+    });
+
+    await Payment.build({
+      orderId: order.id,
+      stripeId: charge.id,
+    }).save();
 
     res.send({ success: true });
   }
